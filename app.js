b6c198585afe51b88417fef93aebced1ad932d99
beytullahbyram <beytullah_bayram67@hotmail.com>
2022-05-06 15:48:14 +0300

    first  commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('index sayfası')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Uygulama ${port} portunda başlatıldı`);
+})
