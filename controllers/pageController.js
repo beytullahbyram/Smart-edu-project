4492321b9998fc0b9dcbb3325cab78d94cb05fd0
beytullahbyram <beytullah_bayram67@hotmail.com>
2022-06-02 13:43:56 +0300

    ethereal email use

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,5 +1,6 @@
-//database ile sayfa arasındaki bağlantıyı sağlar
+const nodemailer = require("nodemailer");
 
+//database ile sayfa arasındaki bağlantıyı sağlar
 exports.getIndexPage = (req, res) => {
     console.log(req.session.userıd);
     res.render('index', {
@@ -26,3 +27,59 @@ exports.getLoginPage = (req, res) => {
         page_name: "login"
     })
 }
+
+
+exports.getContact = (req, res) => {
+    res.status(200).render('contact', {
+        page_name: 'contact',
+    });
+};
+
+exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+      page_name: 'contact',
+    });
+  };
+  
+  exports.sendEmail = async (req, res) => {
+  
+    const outputMessage = `
+    
+    <h1>Mail Details </h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
+    console.log(outputMessage);
+    console.log("-------");
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+          user: 'viviane.howe56@ethereal.email',
+          pass: 'A1Su1SH1D2jFKmdyYw'
+      }
+  });
+console.log("-------");
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <viviane.howe56@ethereal.email>', // sender address
+    to: "viviane.howe56@ethereal.email", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: outputMessage
+  });
+  console.log("-------");
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.redirect('contact');
+    res.end()
+  };
